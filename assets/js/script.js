54827cadf8ff584975f138f16360016f1c66418b
Magali <magali.buisseret@gmail.com>
2018-07-24 21:44:30 -0400

    JS-primera parte

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
    $(".col__der").on("click", ".tweet__contenido-like", function(e){
-        $(".fa-heart").addClass("tweet__contenido-like-red");
-        var count = $(".tweet__contenido-contador").children().text();
+        $(this).children().addClass("tweet__contenido-like-red");
+        var count = $(this).siblings("div").children().text();
         count = parseInt(count);
         count = count + 1;
-        $(".tweet__contenido-contador").children().text(count);
+        $(this).siblings("div").children().text(count);
         e.preventDefault();
    })
 
@@ -12,4 +12,17 @@ $(document).ready(function() {
         $(this).parentsUntil(".tweet").remove();
         e.preventDefault();
     })
+    $(".create__contenido--boton").children().on("click", function(e){
+        var usuario = $(".create__contenido--usuario").children().val();
+        var tweeter = $(".create__contenido--texto").children().val();
+        var new_tweet = $('.tweet').children().eq(0).clone();
+        new_tweet.appendTo('.tweet');
+        new_tweet.show();
+        new_tweet.find(".tweet__contenido--usuario").children().text(usuario);
+        new_tweet.find(".tweet__contenido--texto").children().text(tweeter);
+        new_tweet.find(".tweet__contenido-contador").children().text("0");
+        $(".create__contenido--usuario").children().attr("placeholder", "Escribe tu nombre de usuario");
+        $(".create__contenido--texto").children().attr("placeholder", "Escribe tu Tweet");
+        e.preventDefault();
+   })
 });
